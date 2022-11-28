4671055e753dd841bbce2876f2845242a7fac174
gkfevd <110854563+gkfevd@users.noreply.github.com>
2022-11-27 23:06:38 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+/*
+    Student Name: Tara Partin
+    File Name: script.js
+    Date: 11/2022
+*/
+
+//Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
+
